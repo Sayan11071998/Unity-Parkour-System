983adc39143562a2e5a573f5f1b35746af7847bc
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2025-07-17 22:30:38 +0530

    Collision and Gravity Added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,15 +5,25 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float rotationSpeed = 500f;
 
+    [Header("Ground Check Settings")]
+    [SerializeField] private float groundCheckRadius = 0.2f;
+    [SerializeField] private Vector3 groundCheckOffset;
+    [SerializeField] private LayerMask groundLayer;
+
+    private bool isGrounded;
     private Quaternion targetRotation;
 
     private CameraController cameraController;
     private Animator animator;
+    private CharacterController characterController;
+
+    private float ySpeed;
 
     private void Awake()
     {
         cameraController = Camera.main.GetComponent<CameraController>();
         animator = GetComponent<Animator>();
+        characterController = GetComponent<CharacterController>();
     }
 
     private void Update()
@@ -26,13 +36,38 @@ public class PlayerController : MonoBehaviour
         var moveInput = (new Vector3(h, 0, v)).normalized;
         var moveDir = cameraController.PlanerRotation() * moveInput;
 
+        GroundCheck();
+
+        if (isGrounded)
+        {
+            ySpeed = -0.5f;
+        }
+        else
+        {
+            ySpeed += Physics.gravity.y * Time.deltaTime;
+        }
+
+        var velocity = moveDir * moveSpeed;
+        velocity.y = ySpeed;
+        characterController.Move(velocity * Time.deltaTime);
+
         if (moveAmount > 0)
         {
-            transform.position += moveDir * moveSpeed * Time.deltaTime;
             targetRotation = Quaternion.LookRotation(moveDir);
         }
 
         transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
         animator.SetFloat("moveAmount", moveAmount, 0.2f, Time.deltaTime);
+    }
+
+    private void GroundCheck()
+    {
+        isGrounded = Physics.CheckSphere(transform.TransformPoint(groundCheckOffset), groundCheckRadius, groundLayer);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(0, 1, 1, 0.5f);
+        Gizmos.DrawSphere(transform.TransformPoint(groundCheckOffset), groundCheckRadius);
     }
 }
