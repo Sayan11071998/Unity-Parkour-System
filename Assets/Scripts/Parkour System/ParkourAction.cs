373c16d0555cd55e17d3e86f65366a749a5d3a17
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2025-07-22 17:52:00 +0530

    Vault Fence Mirroring Created

--- Assets/Scripts/Parkour System/ParkourAction.cs
+++ Assets/Scripts/Parkour System/ParkourAction.cs
@@ -14,15 +14,16 @@ public class ParkourAction : ScriptableObject
 
     [Header("Target Matching")]
     [SerializeField] private bool enableTargetMatching = true;
-    [SerializeField] private AvatarTarget matchBodyPart;
+    [SerializeField] protected AvatarTarget matchBodyPart;
     [SerializeField] private float matchStartTime;
     [SerializeField] private float matchTargetTime;
     [SerializeField] private Vector3 matchPosWeight = new Vector3(0f, 1f, 0f);
 
     public Quaternion TargetRotation { get; set; }
     public Vector3 MatchPos { get; set; }
+    public bool Mirror { get; set; }
 
-    public bool CheckIfPossible(ObstacleHitData hitData, Transform player)
+    public virtual bool CheckIfPossible(ObstacleHitData hitData, Transform player)
     {
         if (!string.IsNullOrEmpty(ObstacleTag) && hitData.forwardHit.transform.tag != ObstacleTag)
             return false;
